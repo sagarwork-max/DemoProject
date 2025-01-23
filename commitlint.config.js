79574d7a95c6eff4f231f67a84d20a4f294191b1
Sagar Rathod <sagarrathod.work2003@gmail.com>
2025-01-23 18:29:57 +0530

    feat(bug): changes
    few changes

--- commitlint.config.js
+++ commitlint.config.js
@@ -12,18 +12,46 @@ module.exports = {
             2,
             "always",
             [
-                "build",
-                "chore",
-                "ci",
-                "docs",
-                "feat",
-                "fix",
-                "perf",
-                "refactor",
-                "revert",
-                "style",
-                "test",
-                "button",
+                'feat', // New feature
+                'fix', // Bug fix
+                'style', // Documentation changes
+                'refactor', // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+                'test', // Code changes that neither fix a bug nor add a feature
+                'docs', // Performance improvement
+                'chore', // Adding missing tests or correcting existing tests
+                'build', // Changes that affect the build system or external dependencies (example scopes: npm)
+                'ci', // Changes to CI configuration files and scripts
+                'revert', // Other changes that don't modify src or test files
+                'perf', // Reverts a previous commit
+                'BREAKING CHANGE',
+                'security',// Security changes
+                'dependency',
+                'merge',
+                'release',
+                'design',// design related changes
+                'cleanup',// code clean 
+                'config',// configuration based changes
+                'init',// initialize new thing
+                'experiment'// new feature experiment
+            ],
+        ],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'setup', // Project setup
+                'config', // Configuration files
+                'deps', // Dependency updates
+                'feature', // Feature-specific changes
+                'bug', // Bug fixes
+                'docs', // Documentation
+                'style', // Code style/formatting
+                'refactor', // Code refactoring
+                'test', // Tests
+                'build', // Build scripts or configuration
+                'ci', // Continuous integration
+                'release', // Release related changes
+                'other', // Other changes
             ],
         ],
         "header-min-length": [2, "always", 10],
